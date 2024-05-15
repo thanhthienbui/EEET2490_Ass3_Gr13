bd1dbdf27f1b73fa2d4fc34ea077e72195c5010d
Ben Bui <104993551+thanhthienbui@users.noreply.github.com>
2024-05-15 15:25:20 +0700

    Update frame.c

--- Task1ScrollImage/kernel/frame.c
+++ Task1ScrollImage/kernel/frame.c
@@ -140,3 +140,24 @@ void drawImageARGB32(int cur_x, int cur_y, int i_width, int i_height,int f_width
 void playVideoARGB32(int cur_x, int cur_y, int i_width, int i_height,int f_width, int f_height, const unsigned long** vid_frame){
     
 }
+
+void displayVideo(int x, int y) {
+    // Add message to prompt the user how to use
+    uart_puts("Video is playing ...\n");
+    uart_puts("Press s to stop ");
+    char character = uart_get_char();
+    // Keep displaying the video until the user press s
+    while (character != 's') {
+        // // loop through all the frame image video
+        for (int i = 0; i < 15; i++) {
+            // display each frame image
+            display_frame_image(video[i], x, y, video_width,
+                                video_height + y);
+            wait_msec(100000);
+        }
+        character = uart_get_char();
+    }
+
+    // Add message to announce the user
+    uart_puts("\nStopping video ...");
+}
