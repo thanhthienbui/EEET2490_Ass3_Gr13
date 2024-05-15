b80779c32ac5206cf456f32e079e507e7f8c2d74
Ben Bui <104993551+thanhthienbui@users.noreply.github.com>
2024-05-15 15:24:00 +0700

    Update frame.h

--- Task1ScrollImage/kernel/frame.h
+++ Task1ScrollImage/kernel/frame.h
@@ -4,3 +4,4 @@ void drawPixelARGB32(int x, int y, unsigned int attr);
 void drawRectARGB32(int x1, int y1, int x2, int y2, unsigned int attr, int fill); 
 void drawImageARGB32(int cur_x, int cur_y, int i_width, int i_height,int f_width, int f_height, const unsigned long* image_bit);
 void display_frame_image(unsigned int image[], int x, int y, int w, int h);
+void displayVideo(int x, int y);
