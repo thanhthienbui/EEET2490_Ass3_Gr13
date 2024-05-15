d68035d2e542241beb9ea964c8d32825297885ce
Kevzinn <kqnguyen2036@gmail.com>
2024-05-15 13:49:09 +0700

    new irq

--- Task-2/src/irq/irq.c
+++ Task-2/src/irq/irq.c
@@ -1,2 +1,45 @@
 #include "irq.h"
+#include "../uart/uart0.h"
+void enable_irq(){  //enable for timer 1
+    // arm cpu
+    // gpu
 
+    
+    
+    // uart0_puts("Enabling interrupt\n");
+    // ENABLE_IRQS_1 = (1 << SYS_TIMER_MATCH_1_INT);
+    // uart0_puts("got here\n");
+
+    // while(!(FIQ_CONTROL & (1<<7))) {    // do nothing if bit not set
+    //     uart0_puts("waiting\n");
+    //     asm volatile("nop");
+    // };
+    // FIQ_CONTROL = 0b1000000<<0;
+
+    // uart0_puts("here\n");
+    // // ENABLE_BASIC_IRQS = (1 << 0);
+    // uart0_puts("PI3\n");
+}
+
+void disable_irq() {
+#ifdef RPI3
+    DISABLE_IRQS_1 = (1 << SYS_TIMER_MATCH_1_INT);
+#else
+    DISABLE_IRQS_1 = (1<<SYS_TIMER_MATCH_1_INT);
+#endif
+
+}
+
+void handle_irq(){
+    uart0_puts("Handling interrupt\n");
+    // check if timer 1 interrupt
+    if (IRQ_PENDING_1 & (1 << SYS_TIMER_MATCH_1_INT)){
+        // clear the interrupt
+        IRQ_BASIC_PENDING &= ~(1 << SYS_TIMER_MATCH_1_INT);
+        // do something
+        uart0_puts("Timer 1 interrupt\n");
+    } else {
+        uart0_puts("failed interrupt\n");
+    }
+
+}
--- Task-2/src/irq/irq.h
+++ Task-2/src/irq/irq.h
@@ -1,22 +1,67 @@
 #ifndef IRQ_H
 #define IRQ_H
+#include "../gpio.h"
 
-#define IRQ_BASE (MMIO_BASE + 0x7E00B000)
+#define IRQ_BASE (MMIO_BASE + (unsigned int)0x7E00B000)
 
 // registers
 #define IRQ_BASIC_PENDING   (*(volatile unsigned int *)(IRQ_BASE + 0x200))
+
+// 31:0 IRQ pending source  from GPU side
 #define IRQ_PENDING_1       (*(volatile unsigned int *)(IRQ_BASE + 0x204))
+// 63:32 IRQ pending source from GPU side
 #define IRQ_PENDING_2       (*(volatile unsigned int *)(IRQ_BASE + 0x208))
+
 #define FIQ_CONTROL         (*(volatile unsigned int *)(IRQ_BASE + 0x20C))
-#define ENABLE_IRQS_1       (*(volatile unsigned int *)(IRQ_BASE + 0x210))
-#define ENABLE_IRQS_2       (*(volatile unsigned int *)(IRQ_BASE + 0x214))
+// IRQ enable 1
+#define ENABLE_IRQS_1       (*(volatile unsigned int *)(IRQ_BASE + 0x210))  //enable interrupt 31:0
+// IRQ enable 2
+#define ENABLE_IRQS_2       (*(volatile unsigned int *)(IRQ_BASE + 0x214))  //enable interrupt 63:32
+// IRQ enable 3
 #define ENABLE_BASIC_IRQS   (*(volatile unsigned int *)(IRQ_BASE + 0x218))
+
 #define DISABLE_IRQS_1      (*(volatile unsigned int *)(IRQ_BASE + 0x21C))
 #define DISABLE_IRQS_2      (*(volatile unsigned int *)(IRQ_BASE + 0x220))
 #define DISABLE_BASIC_IRQS  (*(volatile unsigned int *)(IRQ_BASE + 0x224))
 
+// FIQ source
+// 0-63: GPU interrupts
+// 72-127: Do not use 
+#define FIQ_ARM_TIMER 64
+#define FIQ_ARM_MAILBOX 65
+#define FIQ_ARM_DOORBELL_0 66
+#define FIQ_ARM_DOORBELL_1 67
+#define FIQ_GPU_0_HALT 68
+#define FIQ_GPU_1_HALT 69
+#define FIQ_ILLEGAL_ACCESS_TYPE_1 70
+#define FIQ_ILLEGAL_ACCESS_TYPE_0 71
 
-#define newedit 123
-#define newedit 123
+// ARM peripherals interrupt
+// Timer interrupts
+#define SYS_TIMER_MATCH_1_INT 1
+#define SYS_TIMER_MATCH_3_INT 3
 
+#define USB_CONTROLLER_INT 9
+#define AUX_INT 29
+#define I2C_SPI_SLV_INT 43
+
+#define PWA0_INT 45
+#define PWA1_INT 46
+
+#define SMI_INT 48
+
+#define GPIO_INT_0 49
+#define GPIO_INT_1 50
+#define GPIO_INT_2 51
+#define GPIO_INT_3 52
+
+#define I2C_INT 53
+#define SPI_INT 54
+#define PCM_INT 55
+#define UART_INT 57
+
+// configure for interrupt of timer 1
+void enable_irq();
+void disable_irq();
+void handle_irq();  
 #endif // IRQ_H
