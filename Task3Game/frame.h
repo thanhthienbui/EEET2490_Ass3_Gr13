63d13575580800dee0c7e9b8f21aa7bc2f6f2593
Phạm Huy <104686007+HueyPixelBoxes@users.noreply.github.com>
2024-05-24 15:59:06 +0700

    Delete Task3Game/frame.h

--- Task3Game/frame.h
+++
@@ -1,7 +0,0 @@
-// ----------------------------------- framebf.h ------------------------------------- 
-void framebf_init(int f_width, int f_height); 
-void drawPixelARGB32(int x, int y, unsigned int attr); 
-void drawRectARGB32(int x1, int y1, int x2, int y2, unsigned int attr, int fill); 
-void drawImageARGB32(int cur_x, int cur_y, int i_width, int i_height,int s_width, int s_height, const unsigned long* image_bit);
-void display_frame_image(unsigned int frame_image[], int x, int y, int width, int height);
-void drawLine(int x1, int y1, int x2, int y2, unsigned int attr);
