5d67b2187071f5bc9c6b7dbfaf496dfcc3b9839a
Kevzinn <kqnguyen2036@gmail.com>
2024-05-28 12:36:43 +0700

    Update cmd.c

--- Task3Game/kernel/cmd.c
+++ Task3Game/kernel/cmd.c
@@ -1,16 +1,24 @@
 #include "cmd.h"
 #include "mbox.h"
+#include "image.h"
 #include "utils.h"
 #include "video.h"
 #include "string.h"
 #include "../uart/uart0.h"
 #include "../uart/uart1.h"
-#include "game.h"
 #include "frame.h"
 #include "maze_layout.h"
+#include "sys_timer.h"
 
 
-
+#define RED 0xFFFF0000 // COLOR IN HEXA
+#define WHITE 0xFFFFFFFF
+#define BLUE 0xFF0000FF
+#define CYAN 0xFF00FFFF
+#define YELLOW 0xFFFFFF00
+#define BLACK 0xFF000000
+#define XLIM 40 // MAP LIMIT
+#define YLIM 30
 
 // Game state variables
 int command_count = 0;
@@ -18,6 +26,9 @@ char uart_config[100] = "Default UART Config"; // Placeholder for actual UART co
 void displayOptions() {
     uart_puts(
         "\n\n\tEnter a command:\n"
+        "\t1.\tDisplay text on screen\n"
+        "\t2.\tDisplay image on screen\n"
+        "\t3.\tDisplay a video\n"
         "\t4.\tPlay game\n"
         "\t5.\tClear the screen\n"
     );
@@ -41,7 +52,6 @@ void logCommand(char command) {
     uart_puts("Current UART config: ");
     uart_puts(uart_config);
     uart_puts("\n");
-
 }
 
 // Clear screen (monitor)
@@ -62,7 +72,6 @@ void playFont() {
     drawString(100, 200, "Pham Quang Huy", 0x00FFFF00, 3);
     drawString(100, 300, "Do Manh Huy Hoang", 0x00FF00FF, 3);
     drawString(100, 400, "Bui Hong Thanh Thien", 0x0000FFFF, 3);
-
 }
 
 void drawImage() {
@@ -127,7 +136,6 @@ void drawImage() {
 }
 
 
-
 void playVideo(int x, int y) {
     // set up serial console  
     framebf_init(1024, 768);
@@ -162,8 +170,17 @@ void drawWinGameScreen(int screen_w, int screen_h) {
 }
 
 
+// Function to get value from maze map
+int getMazeValue(int level, int x, int y){
+    return maze_map[level][y * XLIM + x];
+}
 
-void playGame(){
+// Function to set value in maze map
+void setMazeValue(int level, int x, int y, int value) {
+    maze_map[level][y * XLIM + x] = value;
+}
+
+void playGame() {
     int current_level = 0;
     int screen_w = 640;
     int screen_h = 480;
@@ -179,15 +196,15 @@ void playGame(){
     int goal_x = 0;
     int goal_y = 0;
 
-    // Function to get value from maze map
-    int getMazeValue(int level, int x, int y) {
-        return maze_map[level][y * XLIM + x];
-    }
+    // // Function to get value from maze map
+    // int getMazeValue(int level, int x, int y){
+    //     return maze_map[level][y * XLIM + x];
+    // }
 
-    // Function to set value in maze map
-    void setMazeValue(int level, int x, int y, int value) {
-        maze_map[level][y * XLIM + x] = value;
-    }
+    // // Function to set value in maze map
+    // void setMazeValue(int level, int x, int y, int value) {
+    //     maze_map[level][y * XLIM + x] = value;
+    // }
 
     // Find spawn and goal positions
     for (int i = 0; i < XLIM; i++) {
@@ -220,7 +237,7 @@ void playGame(){
         drawRectARGB32(0, 0, screen_w, screen_h, BLACK, 1);
         drawRectARGB32(x1, y1, x2, y2, char_col, 1);
     }
-
+    
     while (1) {
         c = getUart();
         drawRectARGB32(x1, y1, x2, y2, BLACK, 1);
@@ -260,7 +277,7 @@ void playGame(){
             y1 = spawn_y;
             x2 = x1 + steps;
             y2 = y1 + steps;
-        }else {
+        } else {
             x1 = new_x1;
             y1 = new_y1;
             x2 = new_x2;
@@ -305,13 +322,125 @@ void playGame(){
                 drawRectARGB32(i * steps, j * steps, (i + 1) * steps, (j + 1) * steps, color, 1);
             }
         }
-
         drawRectARGB32(x1, y1, x2, y2, char_col, 1);
         wait_msec(100);
     }
 }
 
 
+
+
+void execute() {
+    clearScreen();
+
+    while (1) {
+        displayOptions();
+        uart_puts("MyOS>>> ");
+
+        char command = uart_getc();
+        logCommand(command); // Log the command
+
+        command = uart_getc();
+
+        uart_sendc(command);
+        uart_sendc('\n');
+
+        if (command == '1') {
+            clearScreen();
+            playFont();
+            uart_puts("ACK\n"); // Acknowledge the command
+        }
+        else if (command == '2') {
+            clearScreen();
+            drawImage();
+            uart_puts("ACK\n"); // Acknowledge the command
+        }
+        else if (command == '3')
+        {
+            clearScreen();
+            playVideo(320, 240);
+            uart_puts("ACK\n"); // Acknowledge the command
+        }
+        else if (command == '4')
+        {
+            clearScreen();
+            playGame();
+            uart_puts("ACK\n"); // Acknowledge the command
+        }
+        else if (command == '5')
+        {
+            clearScreen();
+            uart_puts("ACK\n"); // Acknowledge the command
+        }
+        else
+        {
+            uart_puts("NAK\n"); // Negative acknowledgment for invalid command
+            uart_puts("Invalid command. Please enter a command:");
+        }
+    }
+
+}
+
+
+
+#include "cmd.h"
+#include "mbox.h"
+#include "utils.h"
+#include "video.h"
+#include "string.h"
+#include "../uart/uart0.h"
+#include "../uart/uart1.h"
+#include "game.h"
+#include "frame.h"
+
+
+
+
+
+// Game state variables
+int command_count = 0;
+char uart_config[100] = "Default UART Config"; // Placeholder for actual UART config
+void displayOptions() {
+    uart_puts(
+        "\n\n\tEnter a command:\n"
+        "\t4.\tPlay game\n"
+        "\t5.\tClear the screen\n"
+    );
+}
+
+void logCommand(char command) {
+    // Increment the command count
+    command_count++;
+
+    // Log the received command to the UART terminal
+    uart_puts("Command received: ");
+    uart_sendc(command);
+    uart_puts("\n");
+
+    // Display the total number of commands received so far
+    uart_puts("Total commands received: ");
+    uart_dec(command_count);
+    uart_puts("\n");
+
+    // Display the current UART configuration
+    uart_puts("Current UART config: ");
+    uart_puts(uart_config);
+    uart_puts("\n");
+
+}
+
+// Clear screen (monitor)
+void clearScreen()
+{
+    framebf_init(1024, 768);
+    for (int i = 0; i < 1024; i++)
+    {
+        for (int j = 0; j < 768; j++)
+        {
+            drawPixelARGB32(i, j, 0x00000000);
+        }
+    }
+}
 
 
 void execute() {
